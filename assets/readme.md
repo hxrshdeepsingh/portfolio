5327b1e1092bfa088d75b1dfe74e5aa3b2f599a9
hxrshdeep <ishu.cxx@gmail.com>
2023-09-19 01:36:45 -0800

    first commit

---
+++ assets/readme.md
@@ -0,0 +1,44 @@
+// const z = document.getElementById("span-date");
+    // const q = document.getElementById("blux");
+    // const u = document.querySelectorAll(".xe");
+
+    // function x() {
+    //     let time = setInterval(function() {
+    //         let w = new Date();
+    //         let s = w.getSeconds();
+    //         let m = w.getMinutes();
+    //         let h = w.getHours();
+    //         z.innerHTML = `${s} : ${m} : ${h}`;
+    //     }, 1000);
+    // }
+
+    // x();
+
+    // document.addEventListener('mousemove', (event) => {
+    //     const mouseX = event.clientX;
+    //     const mouseY = event.clientY;
+    
+    //     q.style.top = mouseY + 'px';
+    //     q.style.left = mouseX + 'px';
+    //     console.log(mouseX, mouseY);
+    // });
+
+    // u.addEventListener('mouseover', (event) =>{
+    //     q.style.width = '100px'
+    //     q.style.height = '100px'
+    //     q.style.transition = '0.5s'
+    //     q.style.background = 'white'
+    //     q.style.opacity = '0.4'
+    // })
+
+    // u.addEventListener('mouseout', (event) =>{
+    //     q.style.width = '20px'
+    //     q.style.height = '20px'
+    //     q.style.transition = 'left 0.2s ease, top 0.2s ease'
+    //     q.style.background = 'white'
+    //     q.style.transition = '0.5s'
+    //     q.style.border = 'none'
+    //     q.style.opacity = '1'
+    //     q.style.filter = 'none'
+
+    // })
--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-    const z = document.getElementById("span-date");
-    const q = document.getElementById("blux");
-    const u = document.getElementById("name");
+document.addEventListener('DOMContentLoaded', () => {
+	const z = document.getElementById("xe-date");
+	const q = document.getElementById("blux");
+	const u = document.querySelectorAll(".xe");
 
-    function x() {
-        let time = setInterval(function() {
-            let w = new Date();
-            let s = w.getSeconds();
-            let m = w.getMinutes();
-            let h = w.getHours();
-            z.innerHTML = `${s} : ${m} : ${h}`;
-        }, 1000);
-    }
+	function updateTime() {
+		setInterval(function() {
+			let now = new Date();
+			let seconds = now.getSeconds();
+			let minutes = now.getMinutes();
+			let hours = now.getHours();
+			z.innerHTML = `${seconds}:${minutes}:${hours}`;
+		}, 1000);
+	}
 
-    x();
+	updateTime();
 
-    document.addEventListener('mousemove', (event) => {
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-    
-        q.style.top = mouseY + 'px';
-        q.style.left = mouseX + 'px';
-        console.log(mouseX, mouseY);
-    });
+	document.addEventListener('mousemove', (event) => {
+		const mouseX = event.clientX;
+		const mouseY = event.clientY;
 
-    u.addEventListener('mouseover', (event) =>{
-        q.style.width = '100px'
-        q.style.height = '100px'
-        q.style.transition = '0.5s'
-        q.style.background = 'white'
-        q.style.opacity = '0.4'
-    })
+		q.style.top = mouseY - 10 + 'px';
+		q.style.left = mouseX - 10 + 'px';
+		q.style.opacity = '1';
+		console.log(mouseX, mouseY);
+	});
 
-    u.addEventListener('mouseleave', (event) =>{
-        q.style.width = '20px'
-        q.style.height = '20px'
-        q.style.transition = 'left 0.2s ease, top 0.2s ease'
-        q.style.background = 'white'
-        q.style.border = 'none'
-        q.style.opacity = '1'
-        q.style.filter = 'none'
+	u.forEach(element => {
+		element.addEventListener('mouseover', (event) => {
+			q.style.width = '100px';
+			q.style.height = '100px';
+		});
 
-    })
+		element.addEventListener('mouseout', (event) => {
+			q.style.width = '20px';
+			q.style.height = '20px';
+		});
+	});
+});
+
+AOS.init();
